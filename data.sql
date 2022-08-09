7d5a89a357407f46af006ad03c43bfe4b41bd1f3
charlotte-divine <divinemaina@gmail.com>
2022-08-09 14:26:24 +0100

    add the optional changes

--- data.sql
+++ data.sql
@@ -1,7 +1,6 @@
 /* Populate database with sample data. */
 INSERT INTO
     animals(
-        id,
         name,
         date_of_birth,
         escape_attempts,
@@ -9,7 +8,7 @@ INSERT INTO
         weight_kg
     )
 VALUES
-    (1, 'Agumon', '03/02/2020', 0, TRUE, 10.23),
-    (2, 'Gabumon', '15/11/2018', 2, TRUE, 8),
-    (3, 'Pikachu', '07/01/2021', 1, FALSE, 15.04),
-    (4, 'Devimon', '12/05/2017', 5, TRUE, 11);
+    ('Agumon', '03/02/2020', 0, TRUE, 10.23),
+    ('Gabumon', '15/11/2018', 2, TRUE, 8),
+    ('Pikachu', '07/01/2021', 1, FALSE, 15.04),
+    ('Devimon', '12/05/2017', 5, TRUE, 11);
--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-    id INT,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL
 );
+
+DROP TABLE animals;
