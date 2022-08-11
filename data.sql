c2494887e52dbb1316df8ef6740429861381b5ef
charlotte-divine <divinemaina@gmail.com>
2022-08-11 10:30:06 +0100

    add required changes

--- data.sql
+++ data.sql
@@ -8,10 +8,10 @@ INSERT INTO
         weight_kg
     )
 VALUES
-    ('Agumon', '03/02/2020', 0, TRUE, 10.23),
-    ('Gabumon', '15/11/2018', 2, TRUE, 8),
-    ('Pikachu', '07/01/2021', 1, FALSE, 15.04),
-    ('Devimon', '12/05/2017', 5, TRUE, 11);
+    ('Agumon', '2020/02/03', 0, TRUE, 10.23),
+    ('Gabumon', '2018/11/15', 2, TRUE, 8),
+    ('Pikachu', '2021/01/07', 1, FALSE, 15.04),
+    ('Devimon', '2017/05/12', 5, TRUE, 11);
 
     -- Instert new data into animals table.
 INSERT INTO
@@ -23,13 +23,13 @@ INSERT INTO
         weight_kg
     )
 VALUES
-    ('Charmander', '08/02/2020', 0, FALSE, -11.0),
-    ('Plantmon', '15/11/2021', 2, TRUE, -5.7),
-    ('Squirtle', '02/04/1993', 3, FALSE, -12.13),
-    ('Angemon', '12/06/2005', 1, TRUE, -45.0),
-    ('Boarmon', '07/06/2005', 7, TRUE, 20.4),
-    ('Blossom', '13/10/1998', 3, TRUE, 17.0),
-    ('Ditto', '14/05/2022', 4, TRUE, 22.0);
+    ('Charmander', '2020/02/08', 0, FALSE, -11.0),
+    ('Plantmon', '2021/11/15', 2, TRUE, -5.7),
+    ('Squirtle', '1993/04/02', 3, FALSE, -12.13),
+    ('Angemon', '2005/06/12', 1, TRUE, -45.0),
+    ('Boarmon', '2005/06/07', 7, TRUE, 20.4),
+    ('Blossom', '1998/10/13', 3, TRUE, 17.0),
+    ('Ditto', '2022/05/14', 4, TRUE, 22.0);
 
 --- insert new data into owners table
 INSERT INTO
